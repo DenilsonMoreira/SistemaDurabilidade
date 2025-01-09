e6527f4d786925a3ec55dfb08f355c937fe2330f
DenilsonMoreira <denilson.tecn@gmail.com>
2025-01-09 03:23:06 -0300

    melhoria no script

--- uscript.sh
+++ uscript.sh
@@ -2,9 +2,14 @@
 
 docker build -t mm .
 if [ $(docker ps | grep my | wc -l) -gt 0 ]; then
+    echo "Deletando o container"
     docker rm my --force
+    echo "Subindo o container"
 else
     echo "N encontrado"
+    echo "Subindo o container"
 fi
 
 docker run --rm -d -p 80:80 --volume=/home/leticia/Documentos/Denilson/Projetos/SistemaDurabilidade/app:/var/www/html --name my mm
+
+echo "http://localhost"
